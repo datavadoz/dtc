83c79233965fc1265a0fbbea80627ef794e54877
Danh <danhvo.uit@gmail.com>
2023-02-19 05:02:26 +0000

    week_4: Remove redundant parent folder

---
+++ week_4/target/compiled/week_4/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+select *
+from `dtc-de-375513`.`dezoomcamp`.`my_first_dbt_model`
+where id = 1
---
+++ week_4/target/compiled/week_4/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `dtc-de-375513`.`dezoomcamp`.`my_first_dbt_model`
+where id is null
+
+
